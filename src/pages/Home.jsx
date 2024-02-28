c3b5a4fa5b7412907f209fa8e3865b2fa9468edf
mza-wtag <mahfuz.zaman@welldev.io>
2024-02-28 14:57:37 +0600

    if no data then handle the issue logic implements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import NotFound from "@components/NotFound/NotFound";
 import HomeBanner from "@components/HomeBanner/HomeBanner";
-import BlogList from "@components/BlogList/BlogList";
 import FilterBlogs from "@components/FilterBlogs/FilterBlogs";
+import BlogList from "@components/BlogList/BlogList";
 import tags from "@constants/tags.json";
 
 const Home = () => {
   const blogs = useSelector((state) => state.blog);
+
   return (
     <div className="container">
-      <HomeBanner blog={blogs[0]} />
-      <div className="common-title-filter-holder">
-        <h2>Latest Posts</h2>
-        <FilterBlogs tags={tags} />
-      </div>
-      <BlogList blogs={blogs} />
+      {blogs.length === 0 ? (
+        <NotFound text={"No Data Found"} />
+      ) : (
+        <>
+          <HomeBanner blog={blogs[0]} />
+          <div className="common-title-filter-holder">
+            <h2>Latest Posts</h2>
+            <FilterBlogs tags={tags} />
+          </div>
+          <BlogList blogs={blogs} />
+        </>
+      )}
     </div>
   );
 };
+
 export default Home;
